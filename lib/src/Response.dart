c96647b3ef9f71678614aff593b785def00bb6e7
huanghaiyang <huanhai24@sina.com.cn>
2019-07-23 23:37:50 +0800

    rewrite Response & ResponseEntry

--- lib/src/Response.dart
+++ lib/src/Response.dart
@@ -5,14 +5,91 @@ import 'package:Q/src/Context.dart';
 import 'package:Q/src/Request.dart';
 import 'package:Q/src/ResponseEntry.dart';
 
-class Response {
-  Application app;
-  HttpResponse res;
-  Request request;
-  Context ctx;
-  ResponseEntry responseEntry;
+abstract class Response {
+  set app(Application app);
 
-  Response([this.app, this.res, this.request, this.ctx]);
+  set res(HttpResponse res);
 
-  void onerror(Error error) async {}
+  set request(Request request);
+
+  set ctx(Context ctx);
+
+  set responseEntry(ResponseEntry responseEntry);
+
+  Application get app;
+
+  HttpResponse get res;
+
+  Request get request;
+
+  Context get ctx;
+
+  ResponseEntry get responseEntry;
+
+  factory Response([Application app, HttpResponse res, Request request, Context ctx, ResponseEntry responseEntry]) =>
+      _Response(app, res, request, ctx, responseEntry);
+}
+
+class _Response implements Response {
+  Application app_;
+
+  HttpResponse res_;
+
+  Request request_;
+
+  Context ctx_;
+
+  ResponseEntry responseEntry_;
+
+  _Response([this.app_, this.res_, this.request_, this.ctx_, this.responseEntry_]);
+
+  @override
+  ResponseEntry get responseEntry {
+    return this.responseEntry_;
+  }
+
+  @override
+  Context get ctx {
+    return this.ctx_;
+  }
+
+  @override
+  Request get request {
+    return this.request_;
+  }
+
+  @override
+  HttpResponse get res {
+    return this.res_;
+  }
+
+  @override
+  Application get app {
+    return this.app_;
+  }
+
+  @override
+  set responseEntry(ResponseEntry responseEntry) {
+    this.responseEntry_ = responseEntry;
+  }
+
+  @override
+  set ctx(Context ctx) {
+    this.ctx_ = ctx;
+  }
+
+  @override
+  set request(Request request) {
+    this.request_ = request;
+  }
+
+  @override
+  set res(HttpResponse res) {
+    this.res_ = res;
+  }
+
+  @override
+  set app(Application app) {
+    this.app_ = app;
+  }
 }
--- lib/src/ResponseEntry.dart
+++ lib/src/ResponseEntry.dart
@@ -1,9 +1,41 @@
-class ResponseEntry {
+abstract class ResponseEntry {
   // 请求经过处理后实际的结果
-  dynamic result;
+  set result(dynamic result);
 
   // 通过converter转换后的结果
-  dynamic convertedResult;
+  set convertedResult(dynamic convertedResult);
 
-  ResponseEntry([this.result]);
+  get result;
+
+  get convertedResult;
+
+  factory ResponseEntry([dynamic result]) => _ResponseEntry(result);
+}
+
+class _ResponseEntry implements ResponseEntry {
+  dynamic result_;
+
+  dynamic convertedResult_;
+
+  _ResponseEntry([this.result_]);
+
+  @override
+  set convertedResult(dynamic convertedResult) {
+    this.convertedResult_ = convertedResult;
+  }
+
+  @override
+  set result(dynamic result) {
+    this.result_ = result;
+  }
+
+  @override
+  get convertedResult {
+    return this.convertedResult_;
+  }
+
+  @override
+  get result {
+    return this.result_;
+  }
 }
