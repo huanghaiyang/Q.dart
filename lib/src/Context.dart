03708d8208cfc2235578083f2ba0ca529320c728
huanghaiyang <huanhai24@sina.com.cn>
2019-07-23 23:22:07 +0800

    rewrite Context

--- lib/src/Context.dart
+++ lib/src/Context.dart
@@ -7,30 +7,79 @@ import 'package:Q/src/Response.dart';
 import 'package:Q/src/Router.dart';
 import 'package:Q/src/utils/UuidUtil.dart';
 
-class Context {
-  Request request;
-  Response response;
-  Application app;
-  int status;
-  String id;
-  Router router;
-  Map<String, Attribute> attributes = Map();
+abstract class Context {
+  factory Context([Request request, Response response, Application app]) => _Context(request, response, app);
 
-  Context([this.request, this.response, this.app]) {
-    this.status = HttpStatus.ok;
-    this.id = uuid;
+  Request get request;
+
+  Response get response;
+
+  Application get app;
+
+  int get status;
+
+  String get id;
+
+  Router get router;
+
+  Map<String, Attribute> get attributes;
+
+  Map get body;
+
+  Map<String, List<String>> get query;
+
+  List<Cookie> get cookies;
+
+  set app(Application app);
+
+  set router(Router router);
+
+  set status(int status);
+
+  Cookie getBookie(String name);
+
+  Iterable<Cookie> getBookiesBy(String domain);
+
+  Attribute getAttribute(String name);
+
+  Iterable<String> getAttributeNames();
+
+  void setAttribute(String name, dynamic value);
+}
+
+class _Context implements Context {
+  Request request_;
+
+  Response response_;
+
+  Application app_;
+
+  int status_;
+
+  String id_;
+
+  Router router_;
+
+  Map<String, Attribute> attributes_ = Map();
+
+  _Context([this.request_, this.response_, this.app_]) {
+    this.status_ = HttpStatus.ok;
+    this.id_ = uuid;
   }
 
+  @override
   Map get body {
-    return this.request.data;
+    return this.request_.data;
   }
 
+  @override
   Map<String, List<String>> get query {
-    return this.router.query;
+    return this.router_.query;
   }
 
+  @override
   List<Cookie> get cookies {
-    return this.request.req.cookies;
+    return this.request_.req.cookies;
   }
 
   Cookie getBookie(String name) {
@@ -39,23 +88,75 @@ class Context {
     });
   }
 
+  @override
   Iterable<Cookie> getBookiesBy(String domain) {
     return this.cookies.where((cookie) {
       return cookie.domain == domain;
     });
   }
 
+  @override
   Attribute getAttribute(String name) {
-    return this.attributes[name];
+    return this.attributes_[name];
   }
 
+  @override
   Iterable<String> getAttributeNames() {
-    return this.attributes.keys;
+    return this.attributes_.keys;
   }
 
+  @override
   void setAttribute(String name, dynamic value) {
-    this.attributes[name] = Attribute(name, value);
+    this.attributes_[name] = Attribute(name, value);
   }
 
-  void onerror(Error error) async {}
+  @override
+  Map<String, Attribute> get attributes {
+    return this.attributes_;
+  }
+
+  @override
+  Router get router {
+    return this.router_;
+  }
+
+  @override
+  String get id {
+    return this.id_;
+  }
+
+  @override
+  int get status {
+    return this.status_;
+  }
+
+  @override
+  Application get app {
+    return this.app_;
+  }
+
+  @override
+  Response get response {
+    return this.response_;
+  }
+
+  @override
+  Request get request {
+    return this.request_;
+  }
+
+  @override
+  set status(int status) {
+    this.status_ = status;
+  }
+
+  @override
+  set router(Router router) {
+    this.router_ = router;
+  }
+
+  @override
+  set app(Application app) {
+    this.app_ = app;
+  }
 }
