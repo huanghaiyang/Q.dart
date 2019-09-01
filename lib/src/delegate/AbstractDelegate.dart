397a6b3d2a7f6805b31eff6fed593adf955b388b
huanghaiyang <huanhai24@sina.com.cn>
2019-09-01 22:24:21 +0800

    修复bug

--- lib/src/delegate/AbstractDelegate.dart
+++ lib/src/delegate/AbstractDelegate.dart
@@ -1,9 +1,1 @@
-import 'package:Q/src/Application.dart';
-
-abstract class AbstractDelegate {
-  factory AbstractDelegate.from(Application application) => _AbstractDelegate.from(application);
-}
-
-class _AbstractDelegate implements AbstractDelegate {
-  factory _AbstractDelegate.from(Application application) => null;
-}
+abstract class AbstractDelegate {}
